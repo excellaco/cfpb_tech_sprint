5e2e5fbddd5adabad55f324afcce62bd07b0453a
Bryan Cash <bryan_cash@bcash-mac.local>
2020-09-29 23:17:38 -0500

    BAC: Ran Black across repo

--- code/load_data.py
+++ code/load_data.py
@@ -7,9 +7,11 @@ def load_bill_data():
     bill_df = pd.DataFrame(columns=["file_name", "file_text"])
     data_path = os.getenv("WRITE_BILLS_PATH", "../data/final_data/bills/")
     dirpath = Path(data_path)
-    
+
     for filename in dirpath.iterdir():
         file_text = filename.read_text()
-        bill_df = bill_df.append({"file_name": filename.name, "file_text": file_text}, ignore_index=True)
-    
+        bill_df = bill_df.append(
+            {"file_name": filename.name, "file_text": file_text}, ignore_index=True
+        )
+
     return bill_df
